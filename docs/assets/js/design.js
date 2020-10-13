1135082114cf3c86f7be8e99733e7efd40ada090
RaquelGarciaCaja <raquelgc8@gmail.com>
2020-10-13 17:17:34 +0200

    js-nothing important

--- docs/assets/js/design.js
+++ docs/assets/js/design.js
@@ -1,14 +1,13 @@
-'use strict'
+"use strict";
 
-
+// const design = document.querySelectorAll(".design");
+// design.innerHTML = "hola";
 const arrow = document.querySelector(".js-arrow-up-down");
-arrow.innerHTML = ("hola");
 
-
-function arrowMove () {
-    const designPalette = document.querySelector(".js-design__palette");
+function arrowMove() {
+  const designPalette = document.querySelector(".js-design__palette");
 }
 
-arrow.addEventListener('click', arrowMove);
+arrow.addEventListener("click", arrowMove);
 
 console.log(arrow);
--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,5 +1,29 @@
-"use strict";const fr=new FileReader,uploadBtn=document.querySelector(".js__profile-trigger"),fileField=document.querySelector(".js__profile-upload-btn"),profileImage=document.querySelector(".js__profile-image"),profilePreview=document.querySelector(".js__profile-preview");function getImage(e){const r=e.currentTarget.files[0];fr.addEventListener("load",writeImage),fr.readAsDataURL(r)}function writeImage(){profileImage.style.backgroundImage=`url(${fr.result})`,profilePreview.style.backgroundImage=`url(${fr.result})`}function fakeFileClick(){fileField.click()}uploadBtn.addEventListener("click",fakeFileClick),fileField.addEventListener("change",getImage);
+"use strict";
+const fr = new FileReader(),
+  uploadBtn = document.querySelector(".js__profile-trigger"),
+  fileField = document.querySelector(".js__profile-upload-btn"),
+  profileImage = document.querySelector(".js__profile-image"),
+  profilePreview = document.querySelector(".js__profile-preview");
+function getImage(e) {
+  const r = e.currentTarget.files[0];
+  fr.addEventListener("load", writeImage), fr.readAsDataURL(r);
+}
+function writeImage() {
+  (profileImage.style.backgroundImage = `url(${fr.result})`),
+    (profilePreview.style.backgroundImage = `url(${fr.result})`);
+}
+function fakeFileClick() {
+  fileField.click();
+}
+uploadBtn.addEventListener("click", fakeFileClick), fileField.addEventListener("change", getImage);
 
+const designTitle = document.querySelector(".design__title");
 
+function arrowMove() {
+  const designPalette = document.querySelector(".design__palette");
+  designPalette.classList.toggle("hidden");
+}
 
+arrowMove();
 
+designTitle.addEventListener("click", arrowMove);
