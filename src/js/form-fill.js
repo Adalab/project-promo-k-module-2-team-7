fb9c204a36599e38cbd6cfdf84a91d445c2d8e07
RaquelGarciaCaja <raquelgc8@gmail.com>
2020-10-15 23:35:05 +0200

    js new fill

--- src/js/form-fill.js
+++ src/js/form-fill.js
@@ -1,22 +1,28 @@
 "use strict";
 
 // constantes
-const fillInputName = document.querySelector(".js-fill__name");
-const fillInputJob = document.querySelector(".js-fill__job");
+
+const dataElements = {
+  name: "",
+  job: "",
+};
+const fillInputs = document.querySelectorAll(".js-fill");
 const fillName = document.querySelector(".js-name");
 const fillJob = document.querySelector(".js-description");
 
 // coger el valor de los inputs
 
-function changePhoto() {
-  const fillInputNameValue = fillInputName.value;
-  const fillInputJobValue = fillInputJob.value;
-  console.log(fillInputJobValue);
-  fillName.innerHTML = fillInputNameValue;
-  fillJob.innerHTML = fillInputJobValue;
+function paintPhoto(ev) {
+  dataElements[ev.currentTarget.id] = ev.currentTarget.value;
+  for (const dataElement in dataElements) {
+    dataElement.innerHTML;
+  }
+  // fillName.innerHTML = fillInputNameValue;
+  // fillJob.innerHTML = fillInputJobValue;
 }
 
 // listener
 
-fillInputJob.addEventListener("focus", changePhoto);
-fillInputName.addEventListener("focus", changePhoto);
+for (const fillInput of fillInputs) {
+  fillInput.addEventListener("focus", paintPhoto);
+}
