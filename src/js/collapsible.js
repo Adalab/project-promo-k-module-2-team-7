dd6c2950511bdeb32b7eaca7d541adf778b4d0f4
RaquelGarciaCaja <raquelgc8@gmail.com>
2020-10-20 09:56:28 +0200

    add hover in new  branch

--- src/js/collapsible.js
+++ src/js/collapsible.js
@@ -6,43 +6,31 @@ const fillSection = document.querySelector(".js-form-fill");
 const cardSection = document.querySelector(".js-form-card");
 const arrowsUp = document.querySelectorAll(".js-arrow-up");
 
-function hiddenSection(ev) {
-  for (const arrowUp of arrowsUp) {
-    let arrowId = arrowUp.id;
+//
 
-    const click = ev.target.id;
-    if (click == 1) {
-      paletteSection.classList.toggle("hidden");
-      fillSection.classList.add("hidden");
-      cardSection.classList.add("hidden");
-      arrowId.classList.add("icon-up-move");
-    } else if (click == 2) {
-      fillSection.classList.toggle("hidden");
-      paletteSection.classList.add("hidden");
-      cardSection.classList.add("hidden");
-      arrowId.classList.add("icon-up-move");
-    } else if (click == 3) {
-      cardSection.classList.toggle("hidden");
-      paletteSection.classList.add("hidden");
-      fillSection.classList.add("hidden");
-      arrowId.classList.add("icon-up-move");
-    }
+function hiddenSection(ev) {
+  // for (let i = 0; i < arrowsUp.length; i++) {
+  //   let arrowUp = arrowsUp[i].id;
+  //   console.log(arrowUp);
+
+  const clickId = ev.target.id;
+  if (clickId == 1) {
+    paletteSection.classList.toggle("hidden");
+    fillSection.classList.add("hidden");
+    cardSection.classList.add("hidden");
+    // arrowUp.classList.add("icon-up-move");
+  } else if (clickId == 2) {
+    fillSection.classList.toggle("hidden");
+    paletteSection.classList.add("hidden");
+    cardSection.classList.add("hidden");
+    // arrowUp.classList.add("icon-up-move");
+  } else if (clickId == 3) {
+    cardSection.classList.toggle("hidden");
+    paletteSection.classList.add("hidden");
+    fillSection.classList.add("hidden");
+    // arrowUp.classList.add("icon-up-move");
   }
 }
-
-// function arrowMove() {
-//   for (const arrowUp of arrowsUp) {
-//     let arrowId = arrowUp.id;
-//     console.log(arrowId);
-//     if (arrowId == 1) {
-//       arrowUp.classList.add("icon-up-move");
-//     } else if (arrowId == 2) {
-//       arrowUp.classList.add("icon-up-move");
-//     } else if (arrowId == 3) {
-//       arrowUp.classList.add("icon-up-move");
-//     }
-//   }
-// }
 
 for (const formTitle of formTitles) {
   formTitle.addEventListener("click", hiddenSection);
